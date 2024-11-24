a260dfa658471c7ba9a12527f649b98128e18966
Amit Singh <a.s.kushwaha5600@gmail.com>
2024-11-25 00:33:03 +0530

    updated commit

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = '<YOUR_KEY>';
+export const KEY = 'f8ab8e49-959f-470a-88c4-283211c73410';
 export const MODAL_CLOSE_SEC = 2.5;
